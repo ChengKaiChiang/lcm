87957774173681aae32d20f92360358c2a4b1410
chengkai <10863105@gm.nfu.edu.tw>
2020-11-23 23:20:55 +0800

    1. app package sweetalert and axios
    2. FOTA.js
    2.1 Select model data when loading the page
    2.2 Select the id of the model to be updated and send it to the backend to update
    
    3. Status.js
    3.1 Generate LCM Status Table when the page loads
    3.2

--- src/pages/FOTA.js
+++ src/pages/FOTA.js
@@ -1,25 +1,93 @@
-import { Table, Col, Row, Button } from 'react-bootstrap';
+import { Table, Row, Button, Form } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 import './css/FOTA.css';
-function lcmstatus() {
+import swal from 'sweetalert';
+
+function LcmStatus() {
+
+    const [count, setCount] = useState([]);
+
+    const [data, setdata] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost/lcm/api/public/index.php/model')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result[0].data);
+                    setdata(result[0].data);
+                }
+            )
+    }, []);
+
+    //改值的時候
+    const onChangeorder = (e) => {
+        const items = [...count];
+        if (e.target.checked === true)
+            items.push(e.target.id);
+        else {
+            items.forEach(function (item, index, arr) {
+                if (item === e.target.id) {
+                    arr.splice(index, 1);
+                }
+            });
+        }
+        setCount(items);
+    }
+
+    function test1() {
+        const items = [...count];
+        console.log(items.length);
+        let model = document.getElementById('model_select').value;
+        var data = new FormData();
+        data.append("id", JSON.stringify(count));
+        data.append("model", model);
+        if (items.length !== 0) {
+            fetch('http://localhost/lcm/api/public/index.php/updateModel', {
+                method: 'POST',
+                body: data
+            }).then((res) => {
+                swal("儲存成功!", "Model已更新!", "success").then(() => {
+                    window.location.reload(false);
+                })
+            })
+                .catch(e => {
+                    console.log(e);
+                })
+        }
+    }
+
     return (
         <div>
-            <Button variant="primary" className="offset-2 mb-3">Firmware Upload</Button>{' '}
+            <Row>
+                <Button variant="primary" className="offset-2 mb-3" onClick={test1}>Firmware Upload</Button>{' '}
+                <Form.Control as="select" className="col-md-1 col-sm-1 ml-3" id="model_select">
+                    {
+                        data.map(data => {
+                            return (
+                                <option>{data.model}</option>
+                            )
+                        })
+                    }
+                </Form.Control>
+            </Row>
+
             <Row>
                 <Table striped bordered className="col-md-8 col-sm-9 col-8 offset-2">
                     <tbody>
                         <tr>
                             <td>
                                 <label for="materialUnchecked"> ID：1</label>
-                                <input type="checkbox" class="checkbox offset-1" />
+                                <input type="checkbox" class="checkbox offset-1" id="1" onChange={(e) => onChangeorder(e)} />
 
                             </td>
                             <td>
                                 <label for="materialUnchecked"> ID：2</label>
-                                <input type="checkbox" class="checkbox offset-1" />
+                                <input type="checkbox" class="checkbox offset-1" id="2" onChange={(e) => onChangeorder(e)} />
                             </td>
                             <td>
                                 <label for="materialUnchecked"> ID：3</label>
-                                <input type="checkbox" class="checkbox offset-1" />
+                                <input type="checkbox" class="checkbox offset-1" id="3" onChange={(e) => onChangeorder(e)} />
                             </td>
                             <td>
                                 <label for="materialUnchecked"> ID：4</label>
@@ -61,4 +129,4 @@ function lcmstatus() {
     );
 }
 
-export default lcmstatus;
+export default LcmStatus;
--- src/pages/Status.js
+++ src/pages/Status.js
@@ -1,107 +1,71 @@
 import { Table, Col, Row, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 import parse from 'html-react-parser';
 import ReactPlayer from 'react-player';
+import axios from "axios";
 
-function lcmstatus() {
+function LcmStatus() {
 
     var a = () => {
         var str = 'Color: BLACK <br/> LCM Power:3.15V <br/> LCM Current:334.4mA <br/> Back Light Power:12.16V <br/> Back Light Current:521.4mA';
         return parse(str);
     }
+
+    const [data, setdata] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost/lcm/api/public/index.php/lcm')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    let a = [];
+                    let x = 0;
+                    for (let i = 0; i < Math.ceil(result[0].data.length / 2); i++) {
+                        if (result[0].data[x + 1] !== undefined)
+                            a[i] = {'ip1': result[0].data[x], 'ip2': result[0].data[x + 1] };
+                        else
+                            a[i] = {'ip1': result[0].data[x], 'ip2': 0 };
+                        x += 2;
+                    }
+                    console.log(a);
+                    console.log(result[0].data);
+                    setdata(a);
+                }
+            )
+    }, []);
+
     return (
         <Row>
-            <div className="offset-2 col-md-4">
+            <div className="col-md-7">
                 <Table striped bordered >
                     <tbody>
-                        <tr>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">1 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">2 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">3 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">4 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">5 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">6 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">7 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">8 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">9 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                            <td>
-                                <Row>
-                                    <Col>
-                                        <span class="offset-1">10 </span>
-                                        <button type="button" class="btn btn-success btn-circle"></button>
-                                    </Col>
-                                </Row>
-                            </td>
-                        </tr>
+                        {data.map((data, index) =>
+                            <tr>
+                                <td>
+                                    <Row>
+                                        <Col>
+                                            <span class="h1">No{data.ip1.id}.</span>
+                                            <button type="button" class="offset-1 btn btn-success btn-circle btn-xl" id={data.ip1.ip}></button>
+                                            <span class="h1 offset-1">{data.ip1.Lcm_model}</span>
+                                        </Col>
+                                    </Row>
+                                </td>
+                                <td>
+                                    <Row>
+                                        <Col>
+                                            <span class="h1">No{data.ip2.id}.</span>
+                                            <button type="button" class="offset-1 btn btn-success btn-circle btn-xl" id={data.ip2.ip}></button>
+                                            <span class="h1 offset-1">{data.ip2.Lcm_model}</span>
+                                        </Col>
+                                    </Row>
+                                </td>
+                            </tr>
+                        )}
                     </tbody>
                 </Table>
 
             </div>
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <Card className="mb-3 col-md-8">
                     <Card.Body>
                         <Card.Title>Device ID: 1</Card.Title>
@@ -121,4 +85,4 @@ function lcmstatus() {
     );
 }
 
-export default lcmstatus;
+export default LcmStatus;
